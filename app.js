108275b93b7b9bfb2c27c5b990d27e8a2ff08f6d
Tarmo Leppänen <tarmo.leppanen@pinja.com>
2023-08-22 00:08:26 +0300

    And after this we should be able to use only GitHub releases \o/

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-async function init() {
-    const data = await (await fetch('data.json')).json();
+const t = (entry, tagName) => entry.getElementsByTagName(tagName)[0];
 
-    updateCurrentStatus(data[data.length - 1]);
-    updateHistory(data);
+async function init() {
+    const atomFeed = (new DOMParser())
+        .parseFromString(
+            await (await fetch('releases.atom')).text(),
+            'text/xml',
+        );
+
+    const items = [...atomFeed.getElementsByTagName('entry')]
+        .map(entry => ({
+            message: t(entry, 'title').textContent,
+            date: t(entry, 'id').textContent.split('/').pop(),
+        }))
+        .reverse();
+
+    updateCurrentStatus(items[items.length - 1]);
+    updateHistory(items);
 }
 
 function updateCurrentStatus(latestIncident) {
